4763811febe6732aec4ca3836793959a298142d1
xml123 <xumingliang@fang88.me>
2019-08-07 08:36:38 +0800

    add some api

--- bloag/urls.py
+++ bloag/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('api/get_friends', views.getFriends, name='getFriends'),
     path('api/get_head_list', views.getHeaderList, name='getHeaderList'),
     path('api/push_artical', views.pushArtical, name='pushArtical'),
-    path('api/get_artical_list', views.getArticalList, name='getArticalList')
+    path('api/get_artical_list', views.getArticalList, name='getArticalList'),
+    path('api/get_artical_type_list', views.getArticalTypeList, name='getArticalTypeList'),
+    path('api/get_all_artical', views.getAllArtical, name='getAllArtical'),
+    path('api/delete_artical', views.deleatArtical, name='deleatArtical'),
+    path('api/get_draft_artical', views.getDraftArtical, name='getDraftArtical'),
+    path('api/get_artical_id', views.getArticalId, name='getArticalId')
 ]
--- bloag/views.py
+++ bloag/views.py
@@ -57,7 +57,7 @@ def getHeaderList(request):
     else:
         return HttpResponse('It is not a POST request!!!')
 
-#发布文章
+#保存或修改文章
 #POST
 def pushArtical(request):
     if request.method == 'POST':
@@ -67,12 +67,20 @@ def pushArtical(request):
             title = data_string['title']
             content = data_string['content']
             classify_obj = Classify.objects.filter(title=type)
-            print('classify_obj', classify_obj)
+            status = data_string['status']
+            fun = data_string['fun']
             localTime = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-            artical = Artical(title=title, content=content, view=0, status=False, classify_id=classify_obj[0].id,
+            if fun == 'add':
+                artical = Artical(title=title, content=content, view=0, status=status, classify_id=classify_obj[0].id,
                               time=localTime)
-            artical.save()
-
+                artical.save()
+            else:
+                id = data_string['id']
+                artical_obj = Artical.objects.get(id=id)
+                artical_obj.title = title
+                artical_obj.content = content
+                artical_obj.status = status
+                artical_obj.save()
         except Exception as e:
             print(e)
             print('获取前端传回的数据失败', data_string)
@@ -99,7 +107,7 @@ def getArticalList(request):
         classify_type = Classify.objects.filter(type=type)
         list = []
         for item1 in classify_type:
-            artical_list = Artical.objects.filter(classify_id=item1.id)
+            artical_list = Artical.objects.filter(classify_id=item1.id, status=True).order_by("-time")
             for item in artical_list:
                 local_time = item.time
                 list.append({
@@ -112,6 +120,160 @@ def getArticalList(request):
             "code": '200',
             "msg": '成功',
             "data": list,
+        }
+        return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json", charset='utf-8',
+                            status='200', reason='success')
+    else:
+        return HttpResponse('It is not a POST request!!!')
+
+#获取指定类型文章
+#POST
+def getArticalTypeList(request):
+    if request.method == 'POST':
+        data_string = json.loads(request.body)
+        try:
+            type = data_string['type']
+        except Exception as e:
+            print(e)
+            print('获取前端传回的数据失败', data_string)
+            return
+        classify_type = Classify.objects.filter(title=type)
+        artical_list = Artical.objects.filter(classify_id=classify_type[0].id, status=True).order_by("-id")
+        list = []
+        for item in artical_list:
+            local_time = item.time
+            list.append({
+                'id': item.id,
+                'title': item.title,
+                'content': item.content,
+                'view': item.view,
+                'time': local_time.strftime('%Y-%m-%d')
+            })
+        data = {
+            "code": '200',
+            "msg": '成功',
+            "data": list,
+        }
+        return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json", charset='utf-8',
+                            status='200', reason='success')
+    else:
+        return HttpResponse('It is not a POST request!!!')
+
+#获取所有文章
+#POST
+def getAllArtical(request):
+    data_string = json.loads(request.body)
+    try:
+        pageNum = data_string['pageNum']
+    except Exception as e:
+        print(e)
+        print('获取前端传回的数据失败')
+    articalList = Artical.objects.filter(status=True).order_by("-time")
+    pageSize = 5
+    paginator = Paginator(articalList, pageSize)
+    total = paginator.count
+    all_pages = paginator.num_pages
+    if int(pageNum) > all_pages:
+        articalList = []
+    else:
+        try:
+            articalList = paginator.page(pageNum)
+        except PageNotAnInteger:
+            articalList = paginator.page(1)
+        except EmptyPage:
+            articalList = paginator.page(paginator.num_pages)
+    list = []
+    for item in articalList:
+        local_time = item.time
+        classify_type = Classify.objects.filter(id=item.classify_id)
+        list.append({
+            'id': item.id,
+            'title': item.title,
+            'content': item.content,
+            'view': item.view,
+            'time': local_time.strftime('%Y-%m-%d'),
+            'type': classify_type[0].type
+        })
+    data = {
+        "code": '200',
+        "msg": '成功',
+        "data": list,
+        "total": total
+    }
+    return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json", charset='utf-8', status='200', reason='success')
+
+#删除文章
+#POST
+def deleatArtical(request):
+    if request.method == 'POST':
+        data_string = json.loads(request.body)
+        try:
+            id = data_string['id']
+        except Exception as e:
+            print(e)
+            print('获取前端传回的数据失败')
+        artical_obj = Artical.objects.filter(id=id)
+        artical_obj.delete()
+        data = {
+            "code": '200',
+            "msg": '成功'
+        }
+        return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json", charset='utf-8',
+                            status='200', reason='success')
+    else:
+        return HttpResponse('It is not a POST request!!!')
+
+#获取草稿箱文章
+#POST
+def getDraftArtical(request):
+    if request.method == 'POST':
+        data_string = json.loads(request.body)
+        try:
+            artical_list = Artical.objects.filter(status=False).order_by("-time")
+        except Exception as e:
+            print(e)
+        list = []
+        for item in artical_list:
+            local_time = item.time
+            list.append({
+                'id': item.id,
+                'title': item.title,
+                'content': item.content,
+                'view': item.view,
+                'time': local_time.strftime('%Y-%m-%d')
+            })
+        data = {
+            "code": '200',
+            "msg": '成功',
+            "data": list,
+        }
+        return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json", charset='utf-8',
+                            status='200', reason='success')
+    else:
+        return HttpResponse('It is not a POST request!!!')
+
+#根据id获取文章
+#POST
+def getArticalId(request):
+    if request.method == 'POST':
+        data_string = json.loads(request.body)
+        try:
+            id = data_string['id']
+        except Exception as e:
+            print(e)
+            print('获取前端传回的数据失败')
+        artical_obj = Artical.objects.filter(id=id)
+        classify_obj = Classify.objects.filter(id=artical_obj[0].classify_id)
+        obj = {
+            'id': artical_obj[0].id,
+            'title': artical_obj[0].title,
+            'content': artical_obj[0].content,
+            'type': classify_obj[0].title
+        }
+        data = {
+            "code": "200",
+            "msg":  "成功",
+            "data": obj
         }
         return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json", charset='utf-8',
                             status='200', reason='success')
